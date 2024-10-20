88da67a4e0f330d722acd774015d7595d4bcd517
Georgezq <yosho2mendoza@gmail.com>
2024-10-19 22:33:04 -0500

    add new changes

--- src/app/components/pages/create-story/create-story.component.ts
+++ src/app/components/pages/create-story/create-story.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Component({
   selector: 'app-create-story',
@@ -6,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./create-story.component.css']
 })
 export class CreateStoryComponent {
+
+  addStoryWithText: boolean = false;
+  addStoryWithImage: boolean = false;
+
+  nombre: string = '';
+  foto: string = '';
+  apellidos: string = '';
+
+  seeAddStoryWithText(){
+    this.addStoryWithText = true;
+  }
+
+  
+  constructor(private authService: AuthService){}
+
+  async ngOnInit() {
+    this.obtenerDatosUsuarioLogueado();
+  }
+
+  private obtenerDatosUsuarioLogueado(){
+    this.authService.getUserLogged().subscribe((user: any) => {
+      this.nombre = user.nombre;
+      this.foto = user.foto;
+      this.apellidos = user.apellido;
+      
+    })
+  }
 
 }
