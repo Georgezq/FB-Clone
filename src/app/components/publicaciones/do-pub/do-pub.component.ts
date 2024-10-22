95a70362f46fb355ba3c9140b152a9e8d4d72c92
Georgezq <yosho2mendoza@gmail.com>
2024-10-22 00:49:19 -0500

    image preview in the pub add

--- src/app/components/publicaciones/do-pub/do-pub.component.ts
+++ src/app/components/publicaciones/do-pub/do-pub.component.ts
@@ -13,9 +13,34 @@ export class DoPubComponent {
   @Input() loading: boolean = false;
 
   placeHolderText: string;
+  addFoto: boolean = false;
+  imageUrl: string | ArrayBuffer | null = null;
+  previewPh: boolean = false;
 
   constructor() {
     this.placeHolderText = `¿Qué estás pensando, `;
+  }
+
+  onFileSelected(event: any): void {
+    const file = event.target.files[0];
+    if (file && this.isValidImageFile(file)) {
+      const reader = new FileReader();
+      
+      reader.onload = (e) => {
+        this.imageUrl = e.target?.result || null;
+      };
+
+      reader.readAsDataURL(file);
+      this.addFoto = false;
+      this.previewPh = true;
+    } else {
+      // Aquí podrías mostrar un mensaje de error al usuario
+    }
+  }
+
+  private isValidImageFile(file: File): boolean {
+    const validTypes = ['image/jpeg', 'image/jpg', 'image/png'];
+    return validTypes.includes(file.type);
   }
 
 }
